aab2d36471bce10131043067f6d2e7749d654fa1
JuanMunozBouchard <jp.knap@gmail.com>
2026-07-04 15:59:11 -0400

    feat: se implementan colmnas lastname y enabled

--- database/migrations/projects/Common/0001_01_01_000000_create_users_table.php
+++ database/migrations/projects/Common/0001_01_01_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class() extends Migration {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('lastname');
             $table->string('email')
                 ->unique();
             $table->timestamp('email_verified_at')
@@ -23,6 +24,8 @@ return new class() extends Migration {
                 ->nullable();
             $table->string('locale', 10)
                 ->nullable();
+            $table->boolean('enabled')
+                ->default(true);
             $table->boolean('is_system_user')
                 ->default(false)
                 ->after('locale');
