87b7a1d3ce340782721a67f37ecb91d3e77a940a
JuanMunozBouchard <jp.knap@gmail.com>
2026-07-19 20:40:38 -0400

    feat(SF-02): agregar trait HasApiTokens a App\Models\User
    Habilita createToken()/tokens() sobre el modelo User para emitir y
    validar tokens personales de Sanctum.

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Permission\Traits\HasRoles;
@@ -17,6 +18,7 @@ class User extends Authenticatable
     use Notifiable;
     use HasRoles;
     use LogsActivity;
+    use HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
